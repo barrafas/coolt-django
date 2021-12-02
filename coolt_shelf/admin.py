f16f3d9b4d2a381bc1ed17f20a2d6e0902942678
adamesalles <adamesalles111@gmail.com>
2021-12-01 23:26:03 -0300

    Updating requirements.txt

--- coolt_shelf/admin.py
+++ coolt_shelf/admin.py
@@ -3,11 +3,30 @@ from import_export.admin import ImportExportModelAdmin
 from .models import *
 
 admin.site.register(Creator)
-admin.site.register(Work)
+# admin.site.register(Work)
 
-# @admin.register(Work)
-# class WorkAdmin(ImportExportModelAdmin):
-#     list_display = ('name', 'type', 'publisher', 'released')
-#     search_fields = ('name', 'released', 'publisher')
-#     #date_hierarchy = 'compra'
-#     list_filter = ('type', 'publisher')
+
+@admin.register(Work)
+class WorkAdmin(ImportExportModelAdmin):
+    list_display = ('name', 'type', 'publisher', 'released')
+    search_fields = ('name', 'released', 'publisher')
+    #date_hierarchy = 'compra'
+    list_filter = ('type', 'publisher')
+
+
+@admin.register(Genre)
+class GenreAdmin(ImportExportModelAdmin):
+    list_display = ('genre')
+    search_fields = ('genre')
+
+
+@admin.register(Work_Creator)
+class Work_CreatorAdmin(ImportExportModelAdmin):
+    list_display = ('work_id', 'creator_id')
+    search_fields = ('work_id', 'creator_id')
+
+
+@admin.register(Work_Genre)
+class Work_GenreAdmin(ImportExportModelAdmin):
+    list_display = ('work_id', 'genre_id')
+    search_fields = ('work_id', 'genre_id')
